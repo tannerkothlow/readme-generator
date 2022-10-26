3663d71ad58c8bccc55621475226a8346b919e1b
Tanner Kothlow <tannerkothlow@msn.com>
2022-10-26 09:40:20 -0500

    Linked starter functions and resp answers captured

--- index.js
+++ index.js
@@ -2,22 +2,32 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'Test quesiton 1',
+    'Test quesiton 2'
+];
 
 // TODO: Create a function to write README file
-writeToFile = (fileName, data) => {}
+writeToFile = (fileName, data) => {
+    const {testAnsObj, testAnsObj2} = data
+    console.log(`File name: ${fileName}.\n Test 1: ${testAnsObj}.\n Test 2: ${testAnsObj2}`);
+}
 
 // TODO: Create a function to initialize app
 init = () => {
     inquirer
         .prompt([
             {
-                message: 'Test Question',
+                message: `${questions[0]}`,
                 name: 'testAnsObj',
+            },
+            {
+                message: `${questions[1]}`,
+                name: 'testAnsObj2',
             },
         ])
         .then((response) => {
-            console.log(response)
+            writeToFile('README.md', response);
         })
 }
 
