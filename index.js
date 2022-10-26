33bfbbeeab529aa7b80a72f51624ebb379e639da
Tanner Kothlow <tannerkothlow@msn.com>
2022-10-26 09:30:14 -0500

    Established inquirer questionaire

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
+const fs = require('fs')
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+writeToFile = (fileName, data) => {}
 
 // TODO: Create a function to initialize app
-function init() {}
+init = () => {
+    inquirer
+        .prompt([
+            {
+                message: 'Test Question',
+                name: 'testAnsObj',
+            },
+        ])
+        .then((response) => {
+            console.log(response)
+        })
+}
 
 // Function call to initialize app
 init();
