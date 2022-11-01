3d1a7027b6c5da62a76fff80497160f406f73688
Tanner Kothlow <tannerkothlow@msn.com>
2022-11-01 13:48:41 -0500

    Fixed minor typo in Inquirer instructions

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const questions = [
     'What kind of license should your project have?', // Inquirer multiple choice
     'What command should be run to instal dependencies?', //Default npm i
     'What command should be run to run tests?', //Default npm test
-    'What are the usage instructions for you repo?',
+    'What are the usage instructions for your repo?',
     'How can a user contribute to your repo?'
 ];
 
